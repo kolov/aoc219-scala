7ffac86135f8351fa2a6da1f97af3387bb152f59
Assen Kolov <a.kolov@newmotion.com>
2019-12-03 21:52:38 +0100

    answer2

--- src/main/scala/com/akolov/aoc2019/Day3.scala
+++ src/main/scala/com/akolov/aoc2019/Day3.scala
@@ -12,31 +12,29 @@ object Day3 extends App {
     program
   }
 
-  case class Path(pos: (Int, Int), visited: Set[(Int, Int)])
+  case class Path(pos: (Int, Int), visited: Map[(Int, Int), Int], time: Int) {
+    def nextPos(pos: (Int, Int)) = {
+      visited.get(pos) match {
+        case None    => Path(pos, visited.updated(pos, time + 1), time + 1)
+        case Some(t) => Path(pos, visited.updated(pos, t), time + 1)
+      }
+    }
+  }
+
   val wires = readInput
 
   def wirePoints(wire: List[String]) = {
-    wire.foldLeft(Path((0, 0), Set.empty)) {
-      case (Path((x, y), visited), command) =>
+    wire.foldLeft(Path((0, 0), Map.empty, 0)) {
+      case (path @ Path((x, y), visited, time), command) =>
         val len = command.substring(1).toInt
-        command.charAt(0) match {
-          case 'U' =>
-            Path((x, y + len), visited ++ (1 to len).map { i =>
-              (x, y + i)
-            }.toSet)
-          case 'D' =>
-            Path((x, y - len), visited ++ (1 to len).map { i =>
-              (x, y - i)
-            }.toSet)
-          case 'R' =>
-            Path((x + len, y), visited ++ (1 to len).map { i =>
-              (x + i, y)
-            }.toSet)
-          case 'L' =>
-            Path((x - len, y), visited ++ (1 to len).map { i =>
-              (x - i, y)
-            }.toSet)
+        val steps = (1 to len)
+        val positions = command.charAt(0) match {
+          case 'U' => steps.map(i => (x, y + i))
+          case 'D' => steps.map(i => (x, y - i))
+          case 'R' => steps.map(i => (x + i, y))
+          case 'L' => steps.map(i => (x - i, y))
         }
+        positions.foldLeft(path){ case (path, pos) => path.nextPos(pos)}
     }
   }
 
@@ -45,7 +43,20 @@ object Day3 extends App {
   val path0 = wirePoints(wires(0))
   val path1 = wirePoints(wires(1))
 
-  val answer1 = path0.visited.filter(path1.visited.contains).map(distance).toList.sorted.head
+  private val crossings  = path0.visited.keys.filter(path1.visited.contains)
+
+  val answer1 = crossings
+    .map(distance)
+    .toList
+    .sorted
+    .head
+
+  val answer2 = crossings
+    .map(p =>  path0.visited.get(p).get + path1.visited.get(p).get)
+    .toList
+    .sorted
+    .head
   println(s"Answer2 $answer1")
+  println(s"Answer2 $answer2")
 
 }
